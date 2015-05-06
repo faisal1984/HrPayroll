0d8612b1cc81d74e629febe6de0f203942a55731
faisal1984 <faisal.ahmad@outlook.com>
2015-05-06 08:52:10 -0700

    first commit

--- src/com/java/model/Login.java
+++ src/com/java/model/Login.java
@@ -1,5 +1,33 @@
 package com.java.model;
 
 public class Login {
+	
+	
+	String userid;
+	String password;
+	
+	public Login(String userid, String password) {
+		super();
+		this.userid = userid;
+		this.password = password;
+	}
+
+	public String getUserid() {
+		return userid;
+	}
+
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
 
 }
